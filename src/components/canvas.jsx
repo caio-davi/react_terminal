ea16f5812490708da365b9413d89d0630ddb0d9c
Caio Davi <caio.davi@tamu.edu>
2023-04-24 19:51:47 -0500

    Temp commit: needs to remove markdown component and create our own

--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 // import FontAwesomeIcon from '@fortawesome/fontawesome-free';
 
 const CanvasComponent = (props) => {
   const [editCommentValue, setEditCommentValue] =
-    React.useState('#Placeholder');
-  const [editing, setEditing] = React.useState(false);
+    React.useState('# Placeholder');
+
+  const [focused, setFocused] = React.useState(true);
+  const onFocus = () => setFocused(true);
+  const notOnFocus = () => setFocused(false);
 
   const handleTextInput = (event: any) => {
-    setEditing(true);
     setEditCommentValue(event.target.value);
   };
+
+  console.log(focused);
 
   const canvasContainerStyle = {
     display: 'flex',
@@ -21,12 +26,25 @@ const CanvasComponent = (props) => {
     border: '1px double rgba(0, 0, 0, 0.55)',
   };
 
+  if (focused) {
+    return (
+      <div>
+        <textarea
+          style={canvasContainerStyle}
+          onChange={handleTextInput}
+          onFocus={onFocus}
+          onBlur={notOnFocus}
+        >
+          {editCommentValue}
+          {/* <FontAwesomeIcon icon={'dog'} /> */}
+        </textarea>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <textarea style={canvasContainerStyle} onChange={handleTextInput}>
-        {editCommentValue}
-        {/* <FontAwesomeIcon icon={'dog'} /> */}
-      </textarea>
+    <div onFocus={onFocus} onBlur={notOnFocus} onClick={onFocus}>
+      <ReactMarkdown>{editCommentValue}</ReactMarkdown>
     </div>
   );
 };
